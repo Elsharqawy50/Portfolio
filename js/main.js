a054b919c7a6f99f9f7cb55fe7480715443d10ad
Abdelrahman Elsharqawy <abdelrahman.elsharqawy50@gmail.com>
2022-07-26 23:42:34 +0200

    Projects Section created

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
+// ======= Nav Section ======= //
+let navLinks = document.querySelectorAll(".nav-link");
 
-
-
+navLinks.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    navLinks.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    ele.classList.add("active");
+  });
+});
+// Nav End
 
 // ======= Skills Section ======= //
-let skills = document.querySelector(".skills")
-let progressBar = document.querySelectorAll(".progress-bar")
+let skills = document.querySelector(".skills");
+let progressBar = document.querySelectorAll(".progress-bar");
 
 window.addEventListener("scroll", () => {
-  if (this.scrollY >= skills.offsetTop-500 ) {
-    progressBar.forEach(ele => {
-      ele.style.width = ele.dataset.width
+  if (this.scrollY >= skills.offsetTop - 500) {
+    progressBar.forEach((ele) => {
+      ele.style.width = ele.dataset.width;
     });
   }
-})
+});
 // Skills End
